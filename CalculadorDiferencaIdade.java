e3e148714f313be2cb8634cab857680113a9a4f0
Carolly <carolly.l.santy@gmail.com>
2026-05-16 22:47:13 -0300

    Add files via upload

---
+++ CalculadorDiferencaIdade.java
@@ -0,0 +1,8 @@
+public class CalculadorDiferencaIdade {
+	public static void main(String[] args){
+		int primeiraIdade = 30; 
+		int segundaIdade = 18;
+		int diferencaIdade = primeiraIdade - segundaIdade;
+		System.out.println("a dierenca entre idades e: "+ diferencaIdade);
+	}
+}
