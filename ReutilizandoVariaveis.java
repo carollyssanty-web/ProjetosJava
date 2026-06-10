e6b7a970918bb6790162753145733e11bfba735f
Carolly <carolly.l.santy@gmail.com>
2026-06-10 14:26:56 -0300

    feat: estrutura inicial dos projetos Java

---
+++ ReutilizandoVariaveis.java
@@ -0,0 +1,11 @@
+public class ReutilizandoVariaveis {
+    public static void main(String[] args) {
+        double salario = 5000;
+        double resultado = salario * 0.3;
+        System.out.println("30% e: "+resultado);
+        resultado = salario * 0.15;
+        System.out.println("15% e: "+resultado);
+        resultado = salario * 0.05;
+        System.out.println("5% e: "+resultado);
+    }
+}
