7eeb3776666107dcbefe2b0eed1ff2236d9e096c
Carolly <carolly.l.santy@gmail.com>
2026-06-10 13:41:15 -0300

    feat: estrutura inicial dos projetos de estudo em Java

---
+++ CalculadorDiferencaIdade.java
@@ -0,0 +1,8 @@
+public class CalculadorDiferencaIdade {
+    public static void main(String[] args){
+        int firstAge = 30;
+        int secondAge = 18;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("a dierenca entre idades e: "+ ageDifference);
+    }
+}
---
+++ CalculadoraImposto.java
@@ -0,0 +1,9 @@
+/*Para usar na matemática*/
+public class CalculadoraImposto {
+    public static void main(String[] args) {
+        float salario = 2500.00F;
+        float porcentagem = 30;
+        float porcentagemDoSalario = salario * (porcentagem / 100);
+        System.out.println(porcentagemDoSalario);
+    }
+}
