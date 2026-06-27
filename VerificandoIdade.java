a2997a1367c7f36abbd08be18becdb3051bbe8d1
Carolly <carolly.l.santy@gmail.com>
2026-06-27 20:58:12 -0300

    correção III.. corrigindo o código invertido

--- VerificandoIdade.java
+++ VerificandoIdade.java
@@ -2,10 +2,10 @@ public class VerificandoIdade {
     public static void main(String[] args) {
         int idade = 25;
         // ==, !=, >, >=, <, <=
-        if(idade <= 18) {
-            System.out.println("Ainda não é Adulto");
-        } else {
+        if(idade >= 18) { //Se for maior ou igual a 18
             System.out.println("Adulto");
+        } else {
+            System.out.println("Ainda não é Adulto");
         }
         }
     }
