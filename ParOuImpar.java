c90eaa80565a903076ad49c1fb2c9beea58bf0d1
Carolly <carolly.l.santy@gmail.com>
2026-07-06 12:29:34 -0300

    :

---
+++ ParOuImpar.java
@@ -0,0 +1,10 @@
+public class ParOuImpar {
+    public static void main(String[] args) {
+        int numero = 12;
+        if( (numero % 2) == 0) {
+            System.out.println("PAR: " + (numero % 2));
+        } else {
+            System.out.println("IMPAR: " + (numero % 2));
+        }
+    }
+}
